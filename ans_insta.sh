df4a1a648d81a0a4f34b28ca7086b65629ee60ba
SAK <120071904+sakit333@users.noreply.github.com>
2025-02-11 14:02:48 +0530

    Update ans_insta.sh

--- ans_insta.sh
+++ ans_insta.sh
@@ -1,5 +1,5 @@
-useradd ansible
-passwd ansible
+sudo useradd ansible
+echo "ansible:1234" | sudo chpasswd 
 sudo sed -i '/^root\s\+ALL=(ALL)\s\+ALL/a ansible ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
 sudo sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
