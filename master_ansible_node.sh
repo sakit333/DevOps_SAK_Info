7213b419ee44755d92ae54ff14b8e39c89017632
SAK <120071904+sakit333@users.noreply.github.com>
2025-02-24 11:17:06 +0530

    done

---
+++ master_ansible_node.sh
@@ -0,0 +1,11 @@
+sudo sed -i 's/^#\(inventory\s*=\s*\/etc\/ansible\/hosts\)/\1/' /etc/ansible/ansible.cfg
+sudo sed -i 's/^#\(sudo_user\s*=\s*root\)/\1/' /etc/ansible/ansible.cfg
+echo "/etc/ansible/ansible/cfg done"
+sudo useradd ansible
+echo "user is created"
+echo "ansible:1234" | sudo chpasswd 
+sudo sed -i '/^root\s\+ALL=(ALL)\s\+ALL/a ansible ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
+sudo sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+service sshd restart
+echo "master node configuration is done"
