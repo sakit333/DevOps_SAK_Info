96621d236f8c0b6690cf6323f5a4dcb2b8347187
SAK <120071904+sakit333@users.noreply.github.com>
2025-03-05 10:48:28 +0530

    done

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "prod_ec2" {
   ami           = "ami-03b8adbf322415fd0"
   instance_type = "t2.micro"
   key_name      = "astroids_203"
-  count         = "1"
+  count         = 1
   tags = {
-    Name = "prod_app_server"
+    Name = "prod_app_server-${count.index}"
   }
   security_groups = [aws_security_group.prod_sg.name]
   user_data       = file("ansible_userdata.sh")
@@ -40,8 +40,8 @@ resource "aws_security_group" "prod_sg" {
   }
 }
 output "public_ip" {
-  value = aws_instance.prod_ec2[0].public_ip
+  value = "EC2 Instance Public IP: ${aws_instance.prod_ec2[0].public_ip}"
 }
 output "private_ip" {
-  value = aws_instance.prod_ec2[0].private_ip
+  value = "EC2 Instance Private IP: ${aws_instance.prod_ec2[0].private_ip}"
 }
