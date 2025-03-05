220a51f9eb438f89d40099f169929123e7d9c312
sakit333 <sak528264@gmail.com>
2025-03-05 12:04:02 +0530

    done working for userdata

--- ansible_userdata.sh
+++ ansible_userdata.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-yum update -y
-useradd ansible
-echo "ansible:1234" | chpasswd 
-echo "ansible ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart sshd
-echo "General Node configuration is done"
+sudo yum update -y
+sudo useradd ansible
+echo "user is created"
+echo "ansible:1234" | sudo chpasswd 
+sudo sed -i '/^root\s\+ALL=(ALL)\s\+ALL/a ansible ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
+sudo sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+service sshd restart
+echo "General node configuration is done"
+
